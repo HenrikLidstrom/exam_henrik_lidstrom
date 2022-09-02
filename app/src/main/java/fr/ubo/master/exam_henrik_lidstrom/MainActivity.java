da36cc94291025bef807b9a7e499aec49bbc3436
Henrik Lidstrom <henrik.lidstrom@isen-ouest.yncrea.fr>
2022-09-02 21:08:46 +0200

    Initial commit

---
+++ app/src/main/java/fr/ubo/master/exam_henrik_lidstrom/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.ubo.master.exam_henrik_lidstrom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
